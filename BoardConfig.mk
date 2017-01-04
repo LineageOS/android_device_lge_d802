adda0e2ec4340f31d4291cad0eb099fa0e58f9c3
Nikola Majkić <n1kolaa924@gmail.com>
2017-01-04 09:14:13 +0000

    d802: rebrand
    Change-Id: I3bf31ea0534d2d935b2470eb5b1a6fb8f632606b
    (cherry picked from commit b48979421dda6e73df683986ca8e69fe9f0a6f08)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +17,7 @@
 
 include device/lge/g2-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := cyanogenmod_d802_defconfig
+TARGET_KERNEL_CONFIG := lineageos_d802_defconfig
 
 TARGET_OTA_ASSERT_DEVICE := d802,g2,galbi,d800
 
