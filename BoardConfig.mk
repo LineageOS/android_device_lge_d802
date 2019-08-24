48a3879b8a2a95acf54451e679d88a912cf31c0b
Alessandro Astone <ales.astone@gmail.com>
2019-08-24 20:04:14 +0200

    d802: remove custom product asserts
    Change-Id: I76face609436ca3a306ebd13cbe5d4020464a42b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,5 @@ include device/lge/g2-common/BoardConfigCommon.mk
 
 TARGET_KERNEL_CONFIG := lineageos_d802_defconfig
 
-TARGET_OTA_ASSERT_DEVICE := d802,g2,galbi,d800
-
 G2_DTS_TARGET := msm8974-g2-open_com
 
